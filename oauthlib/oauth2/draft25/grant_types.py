e13db4657e0e1d7397ae6e6f91c9ceeed1903c76
Ib Lundgren <ib.lundgren@gmail.com>
2012-11-04 11:54:09 +0100

    First go at client credentials grant.

--- oauthlib/oauth2/draft25/grant_types.py
+++ oauthlib/oauth2/draft25/grant_types.py
@@ -175,6 +175,10 @@ class ImplicitGrant(GrantTypeBase):
 
 
 class ResourceOwnerPasswordCredentialsGrant(GrantTypeBase):
+    """`Resource Owner Password Credentials Grant`_
+
+    .. _`Resource Owner Password Credentials Grant`: http://tools.ietf.org/html/rfc6749#section-4.3
+    """
 
     def __init__(self, request_validator=None):
         self.request_validator = request_validator or RequestValidator()
@@ -265,17 +269,57 @@ class ResourceOwnerPasswordCredentialsGrant(GrantTypeBase):
 
 
 class ClientCredentialsGrant(GrantTypeBase):
+    """`Client Credentials Grant`_
+
+    .. _`Client Credentials Grant`: http://tools.ietf.org/html/rfc6749#section-4.4
+    """
 
     def __init__(self, request_validator=None):
         self.request_validator = request_validator or RequestValidator()
 
     def create_token_response(self, request, token_handler):
+        """Return token or error in JSON format.
+
+        The client can request an access token using only its client
+        credentials (or other supported means of authentication) when the
+        client is requesting access to the protected resources under its
+        control, or those of another resource owner that have been previously
+        arranged with the authorization server (the method of which is beyond
+        the scope of this specification).
+
+        The client credentials grant type MUST only be used by confidential
+        clients.
+
+            +---------+                                  +---------------+
+            |         |                                  |               |
+            |         |>--(A)- Client Authentication --->| Authorization |
+            | Client  |                                  |     Server    |
+            |         |<--(B)---- Access Token ---------<|               |
+            |         |                                  |               |
+            +---------+                                  +---------------+
+
+                            Figure 6: Client Credentials Flow
+
+        The flow illustrated in Figure 6 includes the following steps:
+
+        (A)  The client authenticates with the authorization server and
+                requests an access token from the token endpoint.
+
+        (B)  The authorization server authenticates the client, and if valid,
+                issues an access token.
+        """
         try:
+            self.request_validator.authenticate_client(request)
             self.validate_token_request(request)
         except errors.OAuth2Error as e:
             return e.json
         return json.dumps(token_handler(request, refresh_token=True))
 
     def validate_token_request(self, request):
-        # validate grant type, scope
-        pass
+        if not getattr(request, 'grant_type'):
+            raise errors.InvalidRequestError('Request is issing grant type.')
+
+        if not request.grant_type == 'client_credentials':
+            raise errors.UnsupportedGrantTypeError()
+
+        self.request_validator.validate_request_scopes(request)
